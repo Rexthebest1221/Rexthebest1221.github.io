214c40667db0e5b2f3f9d8fb905482c6132e19dc
Rexthebest1221 <74471817+Rexthebest1221@users.noreply.github.com>
2024-05-06 21:58:50 -0400

    ddd

--- js/mobile.js
+++ js/mobile.js
@@ -4,7 +4,7 @@
 function adjustViewport() {
     var viewportWidth = window.innerWidth;
     // You can adjust the initial-scale value to zoom out more (e.g., 0.5 for half the original scale)
-    var initialScale = 0.5; // Adjust this value as needed
+    var initialScale = 0.1; // Adjust this value as needed
     document.querySelector("meta[name=viewport]").setAttribute("content", "width=" + viewportWidth + ", initial-scale=" + initialScale);
 }
 
