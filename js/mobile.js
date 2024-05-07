4061216607768175fed0f850fbf18abb8bb608f1
Rexthebest1221 <74471817+Rexthebest1221@users.noreply.github.com>
2024-05-06 21:59:19 -0400

    d

--- js/mobile.js
+++ js/mobile.js
@@ -3,7 +3,6 @@
 // Function to adjust viewport based on device width
 function adjustViewport() {
     var viewportWidth = window.innerWidth;
-    // You can adjust the initial-scale value to zoom out more (e.g., 0.5 for half the original scale)
     var initialScale = 0.1; // Adjust this value as needed
     document.querySelector("meta[name=viewport]").setAttribute("content", "width=" + viewportWidth + ", initial-scale=" + initialScale);
 }
