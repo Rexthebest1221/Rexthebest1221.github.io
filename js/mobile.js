08262bc32fc81ce534b128d2506a0e24c8ee360c
Rexthebest1221 <74471817+Rexthebest1221@users.noreply.github.com>
2024-05-06 22:02:39 -0400

    bruh

--- js/mobile.js
+++ js/mobile.js
@@ -1,15 +1,1 @@
 // mobile.js
-
-// Function to adjust viewport based on device width
-function adjustViewport() {
-    var viewportWidth = window.innerWidth;
-    // Set the initial scale based on the device width
-    var initialScale = viewportWidth / 1024; // Assuming content designed for a width of 1024px
-    // Limit the minimum scale to avoid excessive zoom out
-    initialScale = Math.max(initialScale, 0.5); // Minimum scale of 0.5
-    document.querySelector("meta[name=viewport]").setAttribute("content", "width=device-width, initial-scale=" + initialScale);
-}
-
-// Call the function initially and on window resize
-adjustViewport();
-window.addEventListener("resize", adjustViewport);
