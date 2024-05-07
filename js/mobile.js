318ff7455f0e6d349c86704e28e9e2dce8316c9d
Rexthebest1221 <74471817+Rexthebest1221@users.noreply.github.com>
2024-05-06 22:05:16 -0400

    shootin myself ong

--- js/mobile.js
+++ js/mobile.js
@@ -1,1 +1,10 @@
-// mobile.js
+
+function adjustViewport() {
+    var viewportWidth = window.innerWidth;
+    var initialScale = viewportWidth / 1024; 
+    initialScale = Math.max(initialScale, 0.5);
+    document.querySelector("meta[name=viewport]").setAttribute("content", "width=device-width, initial-scale=" + initialScale);
+}
+
+adjustViewport();
+window.addEventListener("resize", adjustViewport);
